8276ce6206f97c96f054658ee620fa027c0b57fd
Andy Zalit <andyzalit@gmail.com>
2018-08-24 18:04:16 -0700

    Complete redo with some broken stuff

--- albums.js
+++ albums.js
@@ -1,34 +1,34 @@
-// create header, footer, button side, album side, and containers for body and albums
-
-const header = document.createElement('header');
-const container = document.createElement('div');
-const albumContainer = document.createElement('div')
-const albumList = document.createElement('albumList');
-const playList = document.createElement('playList');
-const footer = document.createElement('footer');
-const body = document.querySelector('body');
-
-body.style.margin = '0';
-body.style.padding = '0';
-
-body.appendChild(header);
-footer.innerText = 'click an album to add its tracks to the bin';
-header.style.backgroundColor = '#9B9B8D';
-header.style.height = '15vh';
-
-body.appendChild(container);
-container.style.backgroundColor = '#EEEEEE';
-container.style.height = '70vh';
-container.style.display = 'flex';
-container.appendChild(albumList);
-container.appendChild(playList);
-
-albumList.style.height = '15vh';
-playList.style.background = 'blue'
-playList.style.height = '70vh';
-playList.style.background = 'yellow'
-
-body.appendChild(footer);
-footer.innerText = '© 2018 LiquidTorch Media';
-footer.style.background = '#68685A';
-footer.style.height = '5vh';
+// // create header, footer, button side, album side, and containers for body and albums
+//
+// const header = document.createElement('header');
+// const container = document.createElement('div');
+// const albumContainer = document.createElement('div')
+// const albumList = document.createElement('albumList');
+// const playList = document.createElement('playList');
+// const footer = document.createElement('footer');
+// const body = document.querySelector('body');
+//
+// body.style.margin = '0';
+// body.style.padding = '0';
+//
+// body.appendChild(header);
+// footer.innerText = 'click an album to add its tracks to the bin';
+// header.style.backgroundColor = '#9B9B8D';
+// header.style.height = '15vh';
+//
+// body.appendChild(container);
+// container.style.backgroundColor = '#EEEEEE';
+// container.style.height = '60vh';
+// container.style.display = 'flex';
+// container.appendChild(albumList);
+// container.appendChild(playList);
+//
+// albumList.style.height = '15vh';
+// playList.style.background = 'blue'
+// playList.style.height = '70vh';
+// playList.style.background = 'yellow'
+//
+// body.appendChild(footer);
+// footer.innerText = '© 2018 LiquidTorch Media';
+// footer.style.background = '#68685A';
+// footer.style.height = '5vh';
--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-// create header, footer, button side, album side, and containers for body and albums
-
-const header = document.createElement('header');
-const container = document.createElement('div');
-const albumContainer = document.createElement('div')
-const buttonSide = document.createElement('buttonSide');
-const trainSide = document.createElement('trainSide');
-const footer = document.createElement('footer');
-const body = document.querySelector('body');
-
-body.style.margin = '0';
-body.style.padding = '0';
-
-body.appendChild(header);
-header.style.backgroundColor = '#9B9B8D';
-header.style.height = '5vh';
-
-body.appendChild(container);
-container.style.backgroundColor = '#EEEEEE';
-container.style.height = '70vh';
-container.style.display = 'flex';
-container.appendChild(buttonSide);
-container.appendChild(trainSide);
-
-buttonSide.style.width = '70vw';
-trainSide.style.width = '70vw';
-trainSide.style.backgroundImage = 'url(./resources/track.fw.png)';
-trainSide.style.backgroundSize = 'contain';
-trainSide.style.opacity = '0.19';
-
-body.appendChild(footer);
-footer.innerText = '© 2018 LiquidTorch Media';
-footer.style.background = '#68685A';
-footer.style.height = '5vh';
+// // create header, footer, button side, album side, and containers for body and albums
+//
+// const header = document.createElement('header');
+// const container = document.createElement('div');
+// const albumContainer = document.createElement('div')
+// const chooseSide = document.createElement('chooseSide');
+// const trainSide = document.createElement('trainSide');
+// const buttonChoose = document.getElementById('buttonChoose');
+// // const buttonClear = document.createElement('clear tracks');
+// // const buttonSubmit = document.createElement('submit bin');
+// const footer = document.createElement('footer');
+// const body = document.querySelector('body');
+//
+// body.style.margin = '0';
+// body.style.padding = '0';
+//
+// body.appendChild(header);
+// header.style.backgroundColor = '#9B9B8D';
+// header.style.height = '5vh';
+//
+// body.appendChild(container);
+// container.style.backgroundColor = '#EEEEEE';
+// container.style.height = '70vh';
+// container.style.display = 'flex';
+// container.appendChild(chooseSide);
+// container.appendChild(trainSide);
+//
+// chooseSide.style.width = '70vw';
+// chooseSide.innerText = 'track bin';
+// chooseSide.style.font = '50px #450F62 avenir';
+// chooseSide.innerTextAlign = 'center';
+// // chooseSide.appendChild(buttonChoose);
+// // buttonChoose.type = 'button';
+// // buttonChoose.value = 'url(./playlist.html)';
+//
+// trainSide.style.width = '70vw';
+// trainSide.style.backgroundImage = 'url(./resources/track.fw.png)';
+// trainSide.style.backgroundSize = 'contain';
+// trainSide.style.opacity = '0.19';
+//
+// body.appendChild(footer);
+// footer.innerText = '© 2018 LiquidTorch Media';
+// footer.style.background = '#68685A';
+// footer.style.height = '5vh';
